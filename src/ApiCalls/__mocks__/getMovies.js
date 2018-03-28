c7f7a8d311abe613d7f3089e8486b01dfb32320e
Patrick McLaughlin <patrickmc21@gmail.com>
2018-03-28 09:52:37 -0600

    Add App tests, remove registerServiceWorker

---
+++ src/ApiCalls/__mocks__/getMovies.js
@@ -0,0 +1,5 @@
+import { cleanedMovie } from '../unit-test/mockData';
+
+export const getMovies = jest.fn().mockImplementation(() => {
+  return Promise.resolve({cleanedMovie})
+});
--- src/Containers/App/App.test.js
+++ src/Containers/App/App.test.js
@@ -1,9 +1,50 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import { App, mapDispatchToProps } from './App';
+import { shallow } from 'enzyme';
+import { getMovies } from '../../ApiCalls/getMovies';
+import { cleanedMovie } from '../../ApiCalls/unit-test/mockData';
+import * as actions from '../../Actions';
+jest.mock('../../ApiCalls/getMovies');
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+
+describe('App', () => {
+
+  let wrapper;
+  let mockRetrieveMovies;
+
+  beforeEach(() => {
+    mockRetrieveMovies = jest.fn();
+    wrapper = shallow(
+      <App retrieveMovies={mockRetrieveMovies}/>, 
+      {disableLifecycleMethods: true});
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call getMovies on componentDidMount', () => {
+    wrapper.instance().componentDidMount();
+    expect(getMovies).toHaveBeenCalled();
+  });
+
+  it('should call retrieveMovies on componentDidMount', async () => {
+    await wrapper.instance().componentDidMount();
+    expect(mockRetrieveMovies).toHaveBeenCalled();
+  });
+});
+
+describe('mapDispatchToProps', () => {
+
+  it('should call dispatch with correct params', () => {
+    const mockDispatch = jest.fn();
+    const mockMovies = cleanedMovie;
+    const expected = actions.postMovies(mockMovies);
+    const mapped = mapDispatchToProps(mockDispatch);
+    mapped.retrieveMovies(mockMovies);
+    expect(mockDispatch).toHaveBeenCalledWith(expected);
+
+  })
+})
+
+
