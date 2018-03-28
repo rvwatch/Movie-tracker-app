6f22055c1969fc2b2715b5a9ec9e229b3d143588
Patrick McLaughlin <patrickmc21@gmail.com>
2018-03-28 09:27:56 -0600

    Add CardContainer tests

--- src/Containers/CardContainer/CardContainer.js
+++ src/Containers/CardContainer/CardContainer.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Card from '../../Components/Card/Card';
 
-const CardContainer = props => {
+export const CardContainer = props => {
   const renderCards = props.movies.map(movie => {
-    return <Card {...movie} />;
+    return <Card key={movie.title} {...movie} />;
   });
   return <section>{renderCards}</section>;
 };
 
-const mapStateToProps = state => ({
+export const mapStateToProps = state => ({
   movies: state.movies
 });
 
