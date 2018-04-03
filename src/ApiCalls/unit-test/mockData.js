0c5b6dcccf6d72c766de0437b4a4bee12890dc41
Patrick McLaughlin <patrickmc21@gmail.com>
2018-04-02 23:01:14 -0600

    Add cardContainer tests, add Login tests

--- src/ApiCalls/unit-test/mockData.js
+++ src/ApiCalls/unit-test/mockData.js
@@ -18,13 +18,13 @@ export const mockMovieData = {
 
 export const cleanedMovie = [
   {
-    date: 'Release Date: 2017-12-09',
-    id: 101.350698,
-    image: '/bXrZ5iHBEjH7WMidbUDQ0U2xbmr.jpg',
+    release_date: 'Release Date: 2017-12-09',
+    movie_id: 101.350698,
+    poster_path: '/bXrZ5iHBEjH7WMidbUDQ0U2xbmr.jpg',
     overview:
       "Summary: The tables are turned as four teenagers are sucked into Jumanji's world - pitted against rhinos, black mambas and an endless variety of jungle traps and puzzles. To survive, they'll play as characters from the game.",
     title: 'Jumanji: Welcome to the Jungle',
-    vote: 'Rating: 6.5'
+    vote_average: 'Rating: 6.5'
   }
 ];
 
