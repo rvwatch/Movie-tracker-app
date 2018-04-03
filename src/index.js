f36370695dae7d611e95c397a88bb20f49979705
Ricardo Viera <ricardoclean@gmail.com>
2018-04-03 10:46:19 -0600

    Adds blockbuster video styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import './reset.css';
 import './index.css';
 import App from './Containers/App/App';
 import { Provider } from 'react-redux';
